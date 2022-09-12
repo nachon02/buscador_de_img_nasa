7c0f22507b3fafcf3aed48de05466b184adc19c2
nachon02 <nachoits02@gmail.com>
2022-09-11 22:35:16 -0300

    agrego validaciones y alertas de errores

--- js/space.js
+++ js/space.js
@@ -2,8 +2,17 @@ btnBuscar = document.getElementById("btnBuscar")
 inputBuscar = document.getElementById("inputBuscar")
 
 function showCards(data){
+    
+
+    document.getElementById("contenedor").innerHTML = ``;
+    // console.log(inputBuscar.value);
     if(data.collection.items.length > 0){
-        console.log(data);
+        if(document.getElementById("card-container")){
+            console.log('ya hay busqueda');
+        }else{
+            inputBuscar.classList.remove('is-invalid')
+        document.getElementById("alertB").classList.add("hide")
+        // console.log(data);
         let contenido = `<div id="title" class="text-center">
                 <h2>Resultado de la búsqueda ${inputBuscar.value.toUpperCase()}</h2>
             </div>`
@@ -15,7 +24,7 @@ function showCards(data){
             contenido += `
 
             <div class="col-md-4 scale">
-            <div class="card mb-4 shadow-sm custom-card cursor-active card_hover"> 
+            <div id="card-container" class="card mb-4 shadow-sm custom-card cursor-active card_hover"> 
 
             <a href="${item.links[0].href}" target="_blank">
                 <img class="bd-placeholder-img card-img-top imgNASA" src="${item.links[0].href}">
@@ -33,12 +42,27 @@ function showCards(data){
             document.getElementById("contenedor").innerHTML = contenido;
         }
     }
+    
+}else{
+    let contenido = `<div id="title" class="text-center">
+            <h2>No hay resultados para la búsqueda ${inputBuscar.value.toUpperCase()}</h2>
+        </div>`
+        document.getElementById("contenedor").innerHTML = contenido;
+}
 
 }
 
 btnBuscar.addEventListener("click", async function(){
    
-    const url = "https://images-api.nasa.gov/search?q=" + inputBuscar.value;
+    const url = "https://images-api.nasa.gov/search?q=" + inputBuscar.value.toLowerCase();
+    
+    if(inputBuscar.value == ""){
+        document.getElementById("contenedor").innerHTML = ``;
+        inputBuscar.classList.add('is-invalid')
+        document.getElementById("alertB").classList.remove("hide")
+        
+    }else{
+    
     const getJSONData = await fetch(url)
                                 .then((response) => response.json())
                                 .catch(function (error) {
@@ -46,6 +70,6 @@ btnBuscar.addEventListener("click", async function(){
                                 });
     showCards(getJSONData);
     // console.log(getJSONData);
-
+                            }
         
 })
